67c4326b3ae233aa68dde5ce565aefb18a89c3af
ikomodd <108951291+ikomodd@users.noreply.github.com>
2024-03-05 01:18:19 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+const anterior = document.getElementById("ant")
+const proximo = document.getElementById("prox")
+
+const imagemElement = document.getElementById("StartIMG")
+
+const imagens = [
+  "Midia/CAPA.jpg",
+  "Midia/IMG1.jpg"
+] 
+
+var ImagemAtual = 0
+
+function ProximaImagem() {
+  
+  imagemElement.src = imagens[ImagemAtual]
+  
+}
+
+proximo.addEventListener("click", function() {
+  if (ImagemAtual < imagens.length - 1) {
+    
+    ImagemAtual += 1
+    ProximaImagem()
+    
+  } 
+}) 
+
+anterior.addEventListener("click", function() {
+  if (ImagemAtual > 0) {
+  
+    ImagemAtual -= 1
+    ProximaImagem()
+  
+  } 
+}) 
