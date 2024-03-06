c90a61b0000b68c61900f2195bcf5b27d2060a44
ikomodd <108951291+ikomodd@users.noreply.github.com>
2024-03-05 21:12:46 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const imagemElement = document.getElementById("StartIMG")
 
 const imagens = [
   "Midia/CAPA.jpg",
-  "Midia/IMG1.jpg"
-  "Midia/IMG-20240305-WA0070.jpg" 
+  "Midia/IMG1.jpg", 
+  "Midia/IMG-20240305-WA0070.jpg", 
   "Midia/IMG-20240305-WA0071.jpg" 
 ] 
 
